884452fd82c8f48a90facd4b6def9c45149d2858
Cedric De Boom <cedricdeboom@gmail.com>
2026-03-07 09:42:53 +0100

    Add student stack

---
+++ terraform/modules/student-stack/ec2.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "ec2" {
+  ami                    = var.ec2_ami_id
+  instance_type          = var.ec2_instance_type
+  key_name               = var.ec2_key_pair_name
+  # update vpc_security_group_ids each time for the new course
+  # it is recreated each time with the new id after destroying
+  # infrastructure
+  
+  vpc_security_group_ids = ["sg-09aa47b654a9aea4e"] # sg_kulroai_student_vm
+  iam_instance_profile   = "kulroai_ec2_profile"
+
+  root_block_device {
+    volume_size = var.ec2_root_block_device_size_in_gb
+  }
+
+  tags = {
+    Owner = local.deployer_name
+  }
+
+  # Install Docker by default.
+  user_data = file("${path.module}/user-data.sh")
+}
